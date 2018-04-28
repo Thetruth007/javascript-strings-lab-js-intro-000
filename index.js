815c86e4129973e0cf486c34549094cb9b73399a
rgossomjr@gmail.com <rgossomjr@gmail.com>
2018-04-28 17:04:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var greeting = "Hello, everybody!";
 
-var specialGuest = "Hello, Hello, Neil deGrasse Tyson!!"
+var specialGuest = "Hello, Hello, Hello, Neil deGrasse Tyson!!!"
 
 var greetSpecialGuest = "" + specialGuest + "!";
 
 var topic = "space";
 
-var conversation = "${Let's talk about space.}";
+var conversation = "Let's talk about space.";
